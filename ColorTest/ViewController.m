85079538524d87f943e69446b2d12d196e76e618
steve0503 <leehr76@gmail.com>
2014-01-03 16:33:16 +0900

    Color Test Done

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,16 +9,69 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *actvityIndicator;
+
+- (IBAction)onColorSliderChanged:(id)sender;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+@synthesize redSlider;
+
+@synthesize greenSlider;
+@synthesize blueSlider;
+
+@synthesize colorView;
+
+@synthesize actvityIndicator;
+
+- (IBAction)onColorSliderChanged:(id)sender {
+    
+    if ([actvityIndicator isAnimating]) {
+        [actvityIndicator stopAnimating];
+    }
+    
+    colorView.backgroundColor = [UIColor colorWithRed:redSlider.value green:greenSlider.value blue:blueSlider.value alpha:1.0];
+    
 }
+
+
+
+- (IBAction)PowerOnOff:(id)sender {
+    
+    BOOL isOn = ((UISwitch*)sender).on;
+    
+    redSlider.enabled = isOn;
+    greenSlider.enabled = isOn;
+    blueSlider.enabled = isOn;
+    
+    if (isOn) {
+        [actvityIndicator startAnimating];
+        
+        [self performSelector:@selector(onColorSliderChanged:) withObject:nil afterDelay:1.0];
+    }
+    else
+    {
+        colorView.backgroundColor = [UIColor grayColor];
+    }
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self onColorSliderChanged:nil];
+
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
